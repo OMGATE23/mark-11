d6f1e6aa05c4ec68faeacdd717da4632da53bcbc
omgate <90276220+OMGATE23@users.noreply.github.com>
2021-12-21 23:52:44 +0530

    feat: added a feature of checking dob is lucky or not

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const dateOfBirth = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkNumberButton = document.querySelector("#check-button");
+const messageFinal = document.querySelector("#message")
+checkNumberButton.addEventListener('click', checkDobIsLucky)
 
-checkNumberButton.addEventListener('click', getValues)
+function checkDobIsLucky(){
+    const dob = dateOfBirth.value;
+    const sum = calculateSum(dob);
+    console.log(sum)
+    if (sum % luckyNumber.value == 0){
+        finalMessage("lucky fella")
+    } else{
+        finalMessage("Kya hi hota hai luck?")
+    }
+}
 
-function getValues(){
-    console.log(dateOfBirth.value)
-    console.log(luckyNumber.value)
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum =0;
+    for (let i=0; i < dob.length; i++){
+        sum = sum + Number(dob.charAt(i))
+    }
+    return sum;
+}
+
+function finalMessage(message){
+    messageFinal.innerHTML = message
 }
